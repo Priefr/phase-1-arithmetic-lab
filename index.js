744237554f5b5bf65951a7fcf160c64e7b60a055
Gideon kirui <kipyegongideon99@gmail.com>
2024-03-19 20:33:01 +0300

    arithmetic reviews

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31; 
+let num2 = 2; 
+let multiply = num1 * num2;
+  const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let numbers = new Set([10, 15, 20, 18, 12]);
+const max = Math.max(...numbers);
